0ec3a8821bb20ca40a1b7d50c5992d68b58e636a
Kanak-dev <kanak24.fv@gmail.com>
2023-09-25 10:39:35 +0545

    todos successfully posted to api and retrieved from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Input from "./Component/Input";
+import axios from 'axios';
+import { v4 as uuid } from "uuid";
 
 function App() {
-  const [inputData, setInputData] = React.useState("");
-  const [todo, setTodo] = React.useState([]);
+  const [inputData, setInputData] = useState("");
+  const [todos, setTodo] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const timer = setTimeout(async () => {
+      const response = await axios({
+        method: 'get',
+        url: 'http://127.0.0.1:8000/api/task-list/',
+      })
+      .then(() => "Successfully fetched...")
+      .catch((err) => `Error while fetching data::ERRROR: ${err}`);
+
+      const data = response.data;
+      setLoading(true);
+      console.log(data);
+      setTodo([...data]);
+    }, 250);
+
+    // DEBOUNCING: if the inputData changes earlier than the given timer i.e. '250ms' then the timer is cleared out, and it won't fetch the data...
+    return () => clearTimeout(timer);
+  }, [inputData]);
 
   function handleInput(event) {
     const { value } = event.target;
     setInputData(value);
   }
+
   function addTodo() {
-    setTodo([...todo, inputData]);
+    // setTodo([...todo, inputData]);
+    axios({
+      method: 'post',
+      url: 'http://127.0.0.1:8000/api/task-create/',
+      data: {
+        id: uuid(), // no real use of uuid() here now,
+        title: inputData,
+        completed: false,
+      }
+    })
+    .then(res => 
+      console.log(`Todo successfully added to the server... Response: ${res}`
+      ))
+    .catch(err => 
+      console.log(`Error occurred while adding todo to the server... Error: ${err}`
+    ));
     setInputData("");
   }
+
   function deleteTodo(index) {
-    const newTodo = todo.filter((_, i) => i !== index);
+    const newTodo = todos.filter((_, i) => i !== index);
     setTodo(newTodo);
   }
-  const listEl = todo.map((item, index) => {
+
+  const listEl = todos.map((todo, index) => {
     return (
       <li key={index} className='flex justify-between items-center mb-2'>
-        {item}
+        {todo.title}
         <button
           onClick={() => deleteTodo(index)}
           className='bg-red-500 hover:bg-red-700 text-white py-1 px-2.5 mx-5 rounded'
--- frontend/src/Component/Input.js
+++ frontend/src/Component/Input.js
@@ -1,18 +1,20 @@
 import React from "react";
 
 export default function Input(props) {
+  const {inputValue, inputFunc, addTodoFunc } = props;  // destructuring the props...
+
   return (
     <div className='flex'>
       <input
         // name='work'
-        value={props.inputValue}
-        onChange={props.inputFunc}
+        value={inputValue}
+        onChange={inputFunc}
         type='text'
         placeholder='Add your work'
         className='border border-gray-400 mr-2 px-4 py-2 flex-grow'
       />
       <button
-        onClick={props.addTodoFunc}
+        onClick={addTodoFunc}
         className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
       >
         Add
